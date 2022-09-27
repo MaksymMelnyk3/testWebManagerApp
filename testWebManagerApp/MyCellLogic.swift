868c52745655d1bea5ad70111d150e6dff5b1e25
Maxim <ya.maksimka228@gmail.com>
2022-09-27 13:52:17 +0300

    UserDefaults: create model UserData

--- testWebManagerApp/MyCellLogic.swift
+++ testWebManagerApp/MyCellLogic.swift
@@ -9,27 +9,27 @@ import Foundation
 import WebSocketManager
 
 
-class MyCellLogic: CellLogic{
-    
-    var gameConfig: GameConfigModel?
-    
-    func configure(gameConfig: GameConfigModel) {
-        self.gameConfig = gameConfig
-    }
-    
-    func handleGameUpdate(mapState: GameDataModel) -> [CellActivity]? {
-        guard let currentCell = mapState.data?.cells?.first else { return nil }
-        
-        
-        guard let velocity = (currentCell.velocity?.moveToPosition(target: mapState.data?.food?.first?.position ?? Position(x: 0, y: 0))) else { return nil }
-//        let cellActivity = CellActivity(cellId: currentCell.id ?? "", speed: 0.9, velocity: velocity, growIntention: GrowIntention(eatEfficiency: 0.1, maxSpeed: 0.1, power: 0.1, mass: 0.1, volatilization: 0.1))
-        
-        let cellActivity = CellActivity(cellId: currentCell.id ?? "", speed: 0.9, velocity: velocity )
-        return [cellActivity]
-//        DesiredCellsState(cell: [cellActivity])
-    }
-    
-}
+//class MyCellLogic: CellLogic{
+//
+//    var gameConfig: GameConfigModel?
+//
+//    func configure(gameConfig: GameConfigModel) {
+//        self.gameConfig = gameConfig
+//    }
+//
+//    func handleGameUpdate(mapState: GameDataModel) -> [CellActivity]? {
+//        guard let currentCell = mapState.data?.cells?.first else { return nil }
+//
+//
+//        guard let velocity = (currentCell.velocity?.moveToPosition(target: mapState.data?.food?.first?.position ?? Position(x: 0, y: 0))) else { return nil }
+////        let cellActivity = CellActivity(cellId: currentCell.id ?? "", speed: 0.9, velocity: velocity, growIntention: GrowIntention(eatEfficiency: 0.1, maxSpeed: 0.1, power: 0.1, mass: 0.1, volatilization: 0.1))
+//
+//        let cellActivity = CellActivity(cellId: currentCell.id ?? "", speed: 0.9, velocity: velocity )
+//        return [cellActivity]
+////        DesiredCellsState(cell: [cellActivity])
+//    }
+//
+//}
 
 
 class Fat: CellLogic {
@@ -42,7 +42,7 @@ class Fat: CellLogic {
         fat.configure(gameConfig: gameConfig)
     }
     
-    func handleGameUpdate(mapState: GameDataModel) -> [CellActivity]? {
+    func handleGameUpdate(mapState: UserData) -> [CellActivity]? {
         fat.handleGameUpdate(mapState: mapState)
     }
     
